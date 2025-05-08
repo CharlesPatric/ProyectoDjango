02a0e28b31c220a3ae2cc6d3ee3dc3c8745367a6
charles-test <capacitacion.tecnologia.ia@gmail.com>
2025-05-08 06:06:59 -0400

    'Actualizacion'

--- mi_aplicacion/models.py
+++ mi_aplicacion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=40)
+    correo = models.EmailField(unique=True)
+   
+   
