b20ed10a305882db75395379dbb20f5522e4c623
charles-test <capacitacion.tecnologia.ia@gmail.com>
2025-05-08 21:01:58 -0400

    Agregar un cambio en el footer

--- mi_aplicacion/models.py
+++ mi_aplicacion/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class Estudiante(models.Model):
-    nombre = models.CharField(max_length=40)
+class Curso(models.Model): # Uno    
+    
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    fecha_inicio =models.DateField()
+    
+    def __str__(self):
+        return self.nombre
+    
+class Estudiante(models.Model): # Muchos
+    
+    ci = models.CharField(max_length=15,primary_key=True)
+    nombre = models.CharField(max_length=100)
     correo = models.EmailField(unique=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True) 
+    cursoFk = models.ForeignKey(Curso,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+
+
+    
+    
    
    
